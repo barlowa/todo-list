52297bf62d797fd99a7f64cccb7a6a2ca5cbc32c
alex <abarlow90@gmail.com>
2018-09-25 21:34:53 +0100

    added functionality to cross off todo items.
    uses stateless component with function prop. maps through items with a 'complete' prop.

--- src/components/todoList.js
+++ src/components/todoList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import TodoForm from '../components/todoForm'
+import Todo from '../components/todo'
 
 class TodoList extends React.Component {
 
@@ -11,7 +12,22 @@ class TodoList extends React.Component {
     this.setState({
       todos: [todo, ...this.state.todos]
     })
+  }
 
+  toggleComplete = (id) =>{ 
+    this.setState({
+      todos: this.state.todos.map(todo =>{
+        //mapping through the whole array and finding matching id value.
+        if(todo.id === id){
+          return{
+            ...todo, //spread operator copies the todo object
+            complete: !todo.complete //updates one of the values in the todo object
+          }
+        }else{
+          return todo //if the id doesnt match, just returns the todo. 
+        }
+      })
+    })
   }
 
   render (){
@@ -22,7 +38,11 @@ class TodoList extends React.Component {
         />
         {this.state.todos.map(todo =>{
           return(
-            <div key = {todo.id}>{todo.text}</div>
+            <Todo 
+              key = {todo.id}
+              todo={todo}
+              toggleComplete = { () =>this.toggleComplete(todo.id) }//lamda function to pass in parameter in the map into function prop.
+            />
           )
         })}
        </div>
